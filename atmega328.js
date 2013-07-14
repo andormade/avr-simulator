ed2af64e0d5a5b5048ae05d2bcc3bf45e7716581
Andor Polgar <hngrhorace@andors-macbook.ccr>
2013-07-14 23:28:56 +0200

    Added instructions + fixes

--- atmega328.js
+++ atmega328.js
@@ -3,6 +3,12 @@ var atmega328 = {
 	/** Program counter */
 	PC: 0,
 
+	/** Stack pointer */
+	SP: 8,
+	SPL: 0,
+	SPH: 0,
+
+	/** SREG */
 	sreg: {
 		/** Global interrupt enable */
 		'I': false,
@@ -22,45 +28,55 @@ var atmega328 = {
 		'C': false
 	},
 
+	/* Register space */
 	reg: {
-		/*          MSB                                                                   LSB */
-		'r0':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r1':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r2':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r3':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r4':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r5':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r6':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r7':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r8':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r9':  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r10': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r11': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r12': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r13': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r14': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r15': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		/*          MSB                                                                   LSB */
-		'r16': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r17': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r18': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r19': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r18': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r19': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r20': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r21': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r22': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r23': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r24': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r25': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r26': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r27': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r28': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r29': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r30': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
-		'r31': [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false]
+		/*       MSB                                                                   LSB */
+		0:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		1:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		2:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		3:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		4:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		5:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		6:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		7:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		8:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		9:  [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		10: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		11: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		12: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		13: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		14: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		15: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		/*       MSB                                                                   LSB */
+		16: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		17: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		18: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		19: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		20: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		21: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		22: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		23: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		24: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		25: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		26: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		27: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		28: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		29: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		30: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		31: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false]
 	},
-			
+
+	stack: {
+		0: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		1: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		2: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		3: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		4: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		5: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		6: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false],
+		7: [7: false, 6: false, 5: false, 4: false, 3: false, 2: false, 1: false, 0: false]
+	},
+
 	memory: [],
 
 
@@ -72,67 +88,108 @@ var atmega328 = {
 	 * @param _Rd    Destination register
 	 * @param _Rr
 	 */
-	'and': function(_Rd, _Rr) {
+	 'and': function(_Rd, _Rr) {
 
-		var Rd = this.reg[_Rd];
-		var Rr = this.reg[_Rr];
+	 	var Rd = this.reg[_Rd];
+	 	var Rr = this.reg[_Rr];
 
-		/* Operation: Rd <- Rd && Rr */
-		Rd[0] = Rd[0] && Rr[0];
-		Rd[1] = Rd[1] && Rr[1];
-		Rd[2] = Rd[2] && Rr[2];
-		Rd[3] = Rd[3] && Rr[3];
-		Rd[4] = Rd[4] && Rr[4];
-		Rd[5] = Rd[5] && Rr[5];
-		Rd[6] = Rd[6] && Rr[6];
-		Rd[7] = Rd[7] && Rr[7]; 
+	 	/* Operation: Rd <- Rd && Rr */
+	 	Rd[0] = Rd[0] && Rr[0];
+	 	Rd[1] = Rd[1] && Rr[1];
+	 	Rd[2] = Rd[2] && Rr[2];
+	 	Rd[3] = Rd[3] && Rr[3];
+	 	Rd[4] = Rd[4] && Rr[4];
+	 	Rd[5] = Rd[5] && Rr[5];
+	 	Rd[6] = Rd[6] && Rr[6];
+	 	Rd[7] = Rd[7] && Rr[7]; 
 
-		/* @TODO */
-		this.sreg['S'];
+	 	/* @TODO */
+	 	this.sreg['S'];
 
-		/* Cleared */
-		this.sreg['V'] = false;
+	 	/* Cleared */
+	 	this.sreg['V'] = false;
 
-		/* Set if MSB of the result is set; cleared otherwise. */
-		this.sreg['N'] = Rd[7];
- 
-		/* Set if the result is $00; cleared otherwise. */
-		this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+	 	/* Set if MSB of the result is set; cleared otherwise. */
+	 	this.sreg['N'] = Rd[7];
 
-		/* Program counter */
-		this.PC++;
+	 	/* Set if the result is $00; cleared otherwise. */
+	 	this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+
+	 	/* Program counter */
+	 	this.PC++;
 
 
-		this.reg[_Rd] = Rd; 
-	},
+	 	this.reg[_Rd] = Rd; 
+	 },
+
+
+
+	/**
+	 * Performs the logical AND between the contents of register Rd and a constant 
+	 * and places the result in the destination register Rd.
+	 *
+	 * @param _Rd
+	 * @param K
+	 */
+
+	 'andi': function(_Rd, K) {
+
+	 	var Rd = this.reg[_Rd];
+
+	 	/* Operation: Rd <- Rd && K */
+	 	Rd[0] = Rd[0] && K[0];
+	 	Rd[1] = Rd[1] && K[1];
+	 	Rd[2] = Rd[2] && K[2];
+	 	Rd[3] = Rd[3] && K[3];
+	 	Rd[4] = Rd[4] && K[4];
+	 	Rd[5] = Rd[5] && K[5];
+	 	Rd[6] = Rd[6] && K[6];
+	 	Rd[7] = Rd[7] && K[7];
+
+	 	/* @TODO */
+	 	this.sreg['S'];
+
+	 	/* Cleared */
+	 	this.sreg['V'] = false;
+
+	 	/* Set if MSB of the result is set; cleared otherwise. */
+	 	this.sreg['N'] = Rd[7];
+
+	 	/* Set if the result is $00; cleared otherwise. */
+	 	this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+
+	 	this.PC++;
+
+	 	this.reg[_Rd] = Rd;
+	 },
 
 
 
 	/**
 	 * Clears the Carry Flag (C) in SREG (Status Register).
 	 */
-	'clc': funcion() {
+	 'clc': funcion() {
 
-		/* Carry Flag cleared */
-		this.sreg['C'] = false;	
+	 	/* Carry Flag cleared */
+	 	this.sreg['C'] = false;	
 
-		/* Program counter */
-		this.PC++;
-	},
+	 	/* Program counter */
+	 	this.PC++;
+	 },
 
 
 
 	/**
 	 * Clears the Half Carry Flag (H) in SREG (Status Register). 
 	 */
-	'clh': funcion() {
-		
-		/* Half Carry Flag cleared */
-		this.sreg['H'] = false;	
+	 'clh': funcion() {
 
-		/* Program counter */
-		this.PC++;
-	},
+	 	/* Half Carry Flag cleared */
+	 	this.sreg['H'] = false;	
+
+	 	/* Program counter */
+	 	this.PC++;
+	 },
 
 
 
@@ -142,28 +199,28 @@ var atmega328 = {
 	 * No interrupt will be executed after the CLI instruction, 
 	 * even if it occurs simultaneously with the CLI instruction.
 	 */
-	'cli': funcion() {
-		
-		/* Global Interrupt Flag cleared */
-		this.sreg['I'] = false;	
+	 'cli': funcion() {
 
-		/* Program counter */
-		this.PC++;
-	},
+	 	/* Global Interrupt Flag cleared */
+	 	this.sreg['I'] = false;	
+
+	 	/* Program counter */
+	 	this.PC++;
+	 },
 
 
 
 	/**
 	 * Clears the Negative Flag (N) in SREG (Status Register).
 	 */
-	'cln': funcion() {
-		
-		/* Negative Flag cleared */
-		this.sreg['N'] = false;	
+	 'cln': funcion() {
 
-		/* Program counter */
-		this.PC++;
-	},
+	 	/* Negative Flag cleared */
+	 	this.sreg['N'] = false;	
+
+	 	/* Program counter */
+	 	this.PC++;
+	 },
 
 
 
@@ -174,84 +231,84 @@ var atmega328 = {
 	 *
 	 * @param _Rd    Destination register
 	 */
-	'clr': function(_Rd) {
+	 'clr': function(_Rd) {
 
-		var Rd = this.reg[_Rd];
+	 	var Rd = this.reg[_Rd];
 
-		/* Operation: Rd <- Rd != Rd */
-		Rd[0] = Rd[0] != Rd[0];
-		Rd[1] = Rd[1] != Rd[1];
-		Rd[2] = Rd[2] != Rd[2];
-		Rd[3] = Rd[3] != Rd[3];
-		Rd[4] = Rd[4] != Rd[4];
-		Rd[5] = Rd[5] != Rd[5];
-		Rd[6] = Rd[6] != Rd[6];
-		Rd[7] = Rd[7] != Rd[7];
+	 	/* Operation: Rd <- Rd != Rd */
+	 	Rd[0] = Rd[0] != Rd[0];
+	 	Rd[1] = Rd[1] != Rd[1];
+	 	Rd[2] = Rd[2] != Rd[2];
+	 	Rd[3] = Rd[3] != Rd[3];
+	 	Rd[4] = Rd[4] != Rd[4];
+	 	Rd[5] = Rd[5] != Rd[5];
+	 	Rd[6] = Rd[6] != Rd[6];
+	 	Rd[7] = Rd[7] != Rd[7];
 
-		this.sreg['S'] = false;
-		this.sreg['V'] = false;
-		this.sreg['N'] = false;
-		this.sreg['Z'] = true;
+	 	this.sreg['S'] = false;
+	 	this.sreg['V'] = false;
+	 	this.sreg['N'] = false;
+	 	this.sreg['Z'] = true;
 
-		/* Program counter */
-		this.PC++;
+	 	/* Program counter */
+	 	this.PC++;
 
 
-		this.reg[_Rd] = Rd;
-	},
+	 	this.reg[_Rd] = Rd;
+	 },
 
 
 
 	/**
 	 * Clears the Signed Flag (S) in SREG (Status Register). 
 	 */
-	'cls': function() {
+	 'cls': function() {
 
-		/* Signed Flag cleared */
-		this.sreg['S'] = false;
+	 	/* Signed Flag cleared */
+	 	this.sreg['S'] = false;
 
-		/* Program counter */
-		this.PC++;
-	},
+	 	/* Program counter */
+	 	this.PC++;
+	 },
 
 
 
 	/**
 	 * Clears the T Flag in SREG (Status Register).
 	 */
-	'clt': function() {
+	 'clt': function() {
 
-		/* T Flag cleared */
-		this.sreg['T'] = false;
+	 	/* T Flag cleared */
+	 	this.sreg['T'] = false;
 
-		this.PC++;
-	},
+	 	this.PC++;
+	 },
 
 
 
 	/**
 	 * Clears the Overflow Flag (V) in SREG (Status Register).
 	 */
-	'clv': function() {
+	 'clv': function() {
 
-		/* Overflow Flag cleared */
-		this.sreg['V'] = false;
+	 	/* Overflow Flag cleared */
+	 	this.sreg['V'] = false;
 
-		this.PC++;
-	},
+	 	this.PC++;
+	 },
 
 
 
 	/**
 	 * Clears the Zero Flag (Z) in SREG (Status Register). 
 	 */
-	'clz': function() {
+	 'clz': function() {
 
-		/* Zero Flag cleared */
-		this.sreg['Z'] = false;
+	 	/* Zero Flag cleared */
+	 	this.sreg['Z'] = false;
 
-		this.PC++;
-	},
+	 	this.PC++;
+	 },
 
 
 
@@ -262,39 +319,39 @@ var atmega328 = {
 	 * @param _Rd
 	 * @param _Rr
 	 */
-	'eor': function() {
+	 'eor': function() {
 
-		var Rd = this.reg[_Rd];
-		var Rr = this.reg[_Rr];
+	 	var Rd = this.reg[_Rd];
+	 	var Rr = this.reg[_Rr];
 
-		/* Operation: Rd <- Rd != Rr */
-		Rd[0] = Rd[0] != Rr[0];
-		Rd[1] = Rd[1] != Rr[1];
-		Rd[2] = Rd[2] != Rr[2];
-		Rd[3] = Rd[3] != Rr[3];
-		Rd[4] = Rd[4] != Rr[4];
-		Rd[5] = Rd[5] != Rr[5];
-		Rd[6] = Rd[6] != Rr[6];
-		Rd[7] = Rd[7] != Rr[7]; 
+	 	/* Operation: Rd <- Rd != Rr */
+	 	Rd[0] = Rd[0] != Rr[0];
+	 	Rd[1] = Rd[1] != Rr[1];
+	 	Rd[2] = Rd[2] != Rr[2];
+	 	Rd[3] = Rd[3] != Rr[3];
+	 	Rd[4] = Rd[4] != Rr[4];
+	 	Rd[5] = Rd[5] != Rr[5];
+	 	Rd[6] = Rd[6] != Rr[6];
+	 	Rd[7] = Rd[7] != Rr[7]; 
 
-		/* @TODO */
-		this.sreg['S'];
+	 	/* @TODO */
+	 	this.sreg['S'];
 
-		/* Cleared */
-		this.sreg['V'] = false;
+	 	/* Cleared */
+	 	this.sreg['V'] = false;
 
-		/* Set if MSB of the result is set; cleared otherwise. */
-		this.sreg['N'] = Rd[7];
- 
-		/* Set if the result is $00; cleared otherwise. */
-		this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+	 	/* Set if MSB of the result is set; cleared otherwise. */
+	 	this.sreg['N'] = Rd[7];
 
-		/* Program counter */
-		this.PC++;
+	 	/* Set if the result is $00; cleared otherwise. */
+	 	this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+
+	 	/* Program counter */
+	 	this.PC++;
 
 
-		this.reg[_Rd] = Rd; 
-	},
+	 	this.reg[_Rd] = Rd; 
+	 },
 
 
 
@@ -306,37 +363,59 @@ var atmega328 = {
 	 * @param _Rd
 	 * @param _Rr
 	 */
-	'mov': function(_Rd, _Rr) {
+	 'mov': function(_Rd, _Rr) {
 
-		var Rd = this.reg[_Rd];
-		var Rr = this.reg[_Rr];
+	 	var Rd = this.reg[_Rd];
+	 	var Rr = this.reg[_Rr];
 
-		/* Operation: Rd <- Rr */
-		Rd[0] = Rr[0];
-		Rd[1] = Rr[1];
-		Rd[2] = Rr[2];
-		Rd[3] = Rr[3];
-		Rd[4] = Rr[4];
-		Rd[5] = Rr[5];
-		Rd[6] = Rr[6];
-		Rd[7] = Rr[7];
+	 	/* Operation: Rd <- Rr */
+	 	Rd[0] = Rr[0];
+	 	Rd[1] = Rr[1];
+	 	Rd[2] = Rr[2];
+	 	Rd[3] = Rr[3];
+	 	Rd[4] = Rr[4];
+	 	Rd[5] = Rr[5];
+	 	Rd[6] = Rr[6];
+	 	Rd[7] = Rr[7];
 
-		this.PC++;
+	 	this.PC++;
 
 
-		this.reg[_Rd] = Rd;
-	},
+	 	this.reg[_Rd] = Rd;
+	 },
+
+
+
+	/**
+	 * This instruction makes a copy of one register pair into another register pair. 
+	 * The source register pair Rr+1:Rr is left unchanged, 
+	 * while the destination register pair Rd+1:Rd is loaded with a copy of Rr + 1:Rr.
+	 *
+	 * @param _Rd
+	 * @param _Rr
+	 */
+	 'movw': function(_Rd, _Rr) {
+
+	 	var Rd  = this.reg[_Rd];
+	 	var Rd1 = this.reg[_Rd + 1];
+	 	var Rr  = this.reg[_Rr];
+	 	var Rr1 = this.reg[_Rr + 1];
+
+	 	/* Operation: Rd+1:Rd <- Rr+1:Rr */
+	 	/* @TODO */
+
+	 }
 
 
 
 	/**
 	 * This instruction performs a single cycle No Operation.
 	 */
-	'nop': function() {
+	 'nop': function() {
 
-		/* Incrementing program cointer */
-		this.PC++;
-	},
+	 	/* Incrementing program cointer */
+	 	this.PC++;
+	 },
 
 
 
@@ -349,35 +428,127 @@ var atmega328 = {
 	 */
 	 'or': function() {
 
-		var Rd = this.reg[_Rd];
-		var Rr = this.reg[_Rr];
+	 	var Rd = this.reg[_Rd];
+	 	var Rr = this.reg[_Rr];
 
-		/* Operation: Rd <- Rd || Rr */
-		Rd[0] = Rd[0] || Rr[0];
-		Rd[1] = Rd[1] || Rr[1];
-		Rd[2] = Rd[2] || Rr[2];
-		Rd[3] = Rd[3] || Rr[3];
-		Rd[4] = Rd[4] || Rr[4];
-		Rd[5] = Rd[5] || Rr[5];
-		Rd[6] = Rd[6] || Rr[6];
-		Rd[7] = Rd[7] || Rr[7]; 
+	 	/* Operation: Rd <- Rd || Rr */
+	 	Rd[0] = Rd[0] || Rr[0];
+	 	Rd[1] = Rd[1] || Rr[1];
+	 	Rd[2] = Rd[2] || Rr[2];
+	 	Rd[3] = Rd[3] || Rr[3];
+	 	Rd[4] = Rd[4] || Rr[4];
+	 	Rd[5] = Rd[5] || Rr[5];
+	 	Rd[6] = Rd[6] || Rr[6];
+	 	Rd[7] = Rd[7] || Rr[7]; 
 
-		/* @TODO */
-		this.sreg['S'];
+	 	/* @TODO */
+	 	this.sreg['S'];
 
-		/* Cleared */
-		this.sreg['V'] = false;
+	 	/* Cleared */
+	 	this.sreg['V'] = false;
 
-		/* Set if MSB of the result is set; cleared otherwise. */
-		this.sreg['N'] = Rd[7];
- 
-		/* Set if the result is $00; cleared otherwise. */
-		this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+	 	/* Set if MSB of the result is set; cleared otherwise. */
+	 	this.sreg['N'] = Rd[7];
 
-		/* Program counter */
-		this.PC++;
+	 	/* Set if the result is $00; cleared otherwise. */
+	 	this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+
+	 	/* Program counter */
+	 	this.PC++;
 
 
-		this.reg[_Rd] = Rd; 
+	 	this.reg[_Rd] = Rd; 
 	 }
-};
+
+
+
+	/**
+	 * Performs the logical OR between the contents of register Rd and a constant 
+	 * and places the result in the destination register Rd.
+	 *
+	 * @param _Rd
+	 * @param K
+	 */
+	 'ori': function(_Rd, K) {
+
+	 	var Rd = this.reg[_Rd];
+
+	 	/* Operation: Rd <- Rd v K */
+	 	Rd[0] = Rd[0] || K[0];
+	 	Rd[1] = Rd[1] || K[1];
+	 	Rd[2] = Rd[2] || K[2];
+	 	Rd[3] = Rd[3] || K[3];
+	 	Rd[4] = Rd[4] || K[4];
+	 	Rd[5] = Rd[5] || K[5];
+	 	Rd[6] = Rd[6] || K[6];
+	 	Rd[7] = Rd[7] || K[7];
+
+	 	/* @TODO */
+	 	this.sreg['S'];
+
+	 	/* Cleared */
+	 	this.sreg['V'] = false;
+
+	 	/* Set if MSB of the result is set; cleared otherwise. */
+	 	this.sreg['N'] = Rd[7];
+
+	 	/* Set if the result is $00; cleared otherwise. */
+	 	this.sreg['Z'] = !Rd[7] && !Rd[6] && !Rd[5] && !Rd[4] && !Rd[3] && !Rd[2] && !Rd[1] && !Rd[0];
+
+	 	this.PC++;
+
+
+	 	this.reg[_Rd] = Rd;
+	 },
+
+
+
+	/**
+	 * This instruction loads register Rd with a byte from the STACK. 
+	 * The Stack Pointer is pre-incremented by 1 before the POP.
+     *
+     * @param _Rd
+     */
+     'pop': function(_Rd) {
+
+     	this.SP++;
+
+     	/* Rd <- STACK */
+     	this.reg[_Rd] = this.stack[this.SP];
+
+     	this.PC++;
+     }
+
+
+
+	 /**
+	  * This instruction stores the contents of register Rr on the STACK. 
+	  * The Stack Pointer is post-decremented by 1 after the PUSH.
+	  *
+	  * @param _Rr
+	  */
+	  'push': function(_Rr) {
+
+	  	/* STACK <- Rr */
+	  	this.stack[this.SP] = this.reg[_Rr];
+
+	  	this.SP--;
+
+	  	this.PC++;
+	  }
+
+
+
+	 /**
+	  * This instruction resets the Watchdog Timer. 
+	  * This instruction must be executed within a limited time given by the WD prescaler. 
+	  * See the Watchdog Timer hardware specification.
+	  */
+	  'wd': function() {
+	  	/* @TODO */
+
+	  	/* Operation: WD timer restart. */
+
+	  	this.PC++;
+	  }
+	};
